b95957fc61bc54728791fcc2455afb7f109ae5a0
htchoi1995 <htchoi1995@gmail.com>
2012-12-28 20:18:25 +0900

    add koreanIME fot tablet

--- config/common_tablet.mk
+++ config/common_tablet.mk
@@ -2,3 +2,8 @@
 $(call inherit-product, vendor/thinkingbridge/config/common.mk)
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/thinkingbridge/overlay/common_tablet
+
+# KoreanIME for Tablet
+PRODUCT_COPY_FILES += \
+    vendor/thinkingbridge/prebuilt/common_tablet/apps/KoreanIME.apk:system/app/KoreanIME.apk \
+    vendor/thinkingbridge/prebuilt/common_tablet/lib/libjni_koreanime.so:system/lib/libjni_koreanime.so
