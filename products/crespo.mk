4f91e3b2bcf8aa5cf486aca7c97e0664774a3740
jkl5616 <jkl5616@gmail.com>
2013-02-02 23:24:16 +0900

    Revert "Update Fingerprint for JOP40G"
    This reverts commit 15b11c2dfe2c39f9d90ea3fd8628566bb8da1f7f.

--- products/crespo.mk
+++ products/crespo.mk
@@ -17,4 +17,4 @@ PRODUCT_MODEL := Nexus S
 PRODUCT_MANUFACTURER := Samsung
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=soju BUILD_FINGERPRINT="google/soju/crespo:4.2.1/JOP40G/485486:user/release-keys" PRIVATE_BUILD_DESC="soju-user 4.2.1 JOP40G 485486 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=soju BUILD_FINGERPRINT="google/soju/crespo:4.2.1/JOP40D/485486:user/release-keys" PRIVATE_BUILD_DESC="soju-user 4.2.1 JOP40D 485486 release-keys"
