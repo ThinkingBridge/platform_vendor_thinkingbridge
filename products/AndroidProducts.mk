f9145451dd6c7b95af2dd8d5529eda79c876762a
Black-Prince <shreekant.nagre@gmail.com>
2013-03-21 22:57:44 +0530

    Bringing I9100G to main stream line

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -5,6 +5,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/manta.mk \
     $(LOCAL_DIR)/crespo.mk \
     $(LOCAL_DIR)/i9100.mk \
+    $(LOCAL_DIR)/i9100g.mk \
     $(LOCAL_DIR)/i9300.mk \
     $(LOCAL_DIR)/n7000.mk \
     $(LOCAL_DIR)/n7100.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,6 +4,7 @@ add_lunch_combo thinkingbridge_manta-userdebug
 add_lunch_combo thinkingbridge_grouper-userdebug
 add_lunch_combo thinkingbridge_crespo-userdebug
 add_lunch_combo thinkingbridge_i9100-userdebug
+add_lunch_combo thinkingbridge_i9100g-userdebug
 add_lunch_combo thinkingbridge_i9300-userdebug
 add_lunch_combo thinkingbridge_n7000-userdebug
 add_lunch_combo thinkingbridge_n7100-userdebug
