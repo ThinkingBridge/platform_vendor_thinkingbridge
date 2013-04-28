784dc92be15ff42021995f8148149c58fb025778
htchoi1995 <htchoi1995@gmail.com>
2013-04-29 08:01:35 +0900

    bump version

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,3 +10,4 @@ add_lunch_combo thinkingbridge_i9300-userdebug
 add_lunch_combo thinkingbridge_n7000-userdebug
 add_lunch_combo thinkingbridge_n7100-userdebug
 add_lunch_combo thinkingbridge_pyramid-userdebug
+add_lunch_combo thinkingbridge_e975-userdebug
