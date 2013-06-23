da7f068138003207d10007e42ed7d37da2f27885
htchoi1995 <htchoi1995@gmail.com>
2013-06-23 18:53:14 +0900

    vendor: support olympus (Atrix)

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,3 +11,4 @@ add_lunch_combo thinkingbridge_n7000-userdebug
 add_lunch_combo thinkingbridge_n7100-userdebug
 add_lunch_combo thinkingbridge_pyramid-userdebug
 add_lunch_combo thinkingbridge_e975-userdebug
+add_lunch_combo thinkingbridge_olympus-userdebug
