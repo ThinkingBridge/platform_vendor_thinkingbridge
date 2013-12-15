4364480d225bbbb42d071df736684460f6c97e14
htchoi1995 <htchoi1995@gmail.com>
2013-12-15 13:52:18 +0900

    common: fix shell date

--- config/common.mk
+++ config/common.mk
@@ -2,21 +2,21 @@ PRODUCT_BRAND ?= thinkingbridge
 
 # general properties
 PRODUCT_PROPERTY_OVERRIDES += \
-	keyguard.no_require_sim=true \
-	ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
-	ro.com.google.clientidbase=android-google \
-	ro.com.android.wifi-watchlist=GoogleGuest \
-	ro.setupwizard.enterprise_mode=1 \
-	ro.com.android.dateformat=MM-dd-yyyy \
-	ro.com.android.dataroaming=false \
-	persist.sys.root_access=1
+    keyguard.no_require_sim=true \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.com.android.dataroaming=false \
+    persist.sys.root_access=1
 
 # ThinkingBridge stable version
 # SB = Stable Build <version number> 
 VERSION_MOD_NAME := Stable6
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.thinkingbridge.version=$(TARGET_PRODUCT)_$(VERSION_MOD_NAME)_$(shell date +"%y-%m-%d")
+    ro.thinkingbridge.version=$(TARGET_PRODUCT)_$(VERSION_MOD_NAME)_$(shell date -u +%Y%m%d)
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
