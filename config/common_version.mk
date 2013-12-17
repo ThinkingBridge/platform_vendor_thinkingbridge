02036c633488ce3e94511643acafcef8fbe17871
htchoi1995 <htchoi1995@gmail.com>
2013-12-17 15:41:09 +0900

    vendor: better zip name

--- config/common_version.mk
+++ config/common_version.mk
@@ -2,7 +2,7 @@ BRANCH = kitkat
 DATE = $(shell date -u +%Y%m%d)
 
 #Set ThinkingBridge version
-    THINKINGBRIDGE_VERSION := $(TARGET_PRODUCT)-$(PLATFORM_VERSION)_$(DATE)-nightly
+    THINKINGBRIDGE_VERSION := $(TARGET_PRODUCT)-$(DATE)-nightly
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.thinkingbridge.version=$(THINKINGBRIDGE_VERSION)
