5f7182ece834f38bb015b8fa016e2620736d1daf
htchoi1995 <htchoi1995@gmail.com>
2013-12-18 18:34:23 +0900

    vendor: update common_version.mk

--- config/common_version.mk
+++ config/common_version.mk
@@ -1,8 +1,31 @@
-BRANCH = kitkat
-DATE = $(shell date -u +%Y%m%d)
+# Versioning of the ROM
 
-#Set ThinkingBridge version
-    THINKINGBRIDGE_VERSION := $(TARGET_PRODUCT)-$(DATE)-nightly
+ifdef BUILDTYPE_NIGHTLY
+        ROM_BUILDTYPE := NIGHTLY
+endif
+ifdef BUILDTYPE_WEEKLY
+        ROM_BUILDTYPE := WEEKLY
+endif
+ifdef BUILDTYPE_STABLE
+        ROM_BUILDTYPE := STABLE
+endif
+ifndef ROM_BUILDTYPE
+        ROM_BUILDTYPE := UNOFFICIAL
+endif
+
+TARGET_PRODUCT_SHORT := $(TARGET_PRODUCT)
+TARGET_PRODUCT_SHORT := $(subst thinkingbridge_,,$(TARGET_PRODUCT_SHORT))
+
+# Build the final version string
+ifdef BUILDTYPE_RELEASE
+        THINKINGBRIDGE_VERSION := $(PLATFORM_VERSION)-$(TARGET_PRODUCT_SHORT)
+else
+ifeq ($(ROM_BUILDTIME_LOCAL),y)
+        THINKINGBRIDGE_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d-%H%M%z)-$(TARGET_PRODUCT_SHORT)-$(ROM_BUILDTYPE)
+else
+        THINKINGBRIDGE_VERSION := $(PLATFORM_VERSION)-$(shell date -u +%Y%m%d)-$(TARGET_PRODUCT_SHORT)-$(ROM_BUILDTYPE)
+endif
+endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.thinkingbridge.version=$(THINKINGBRIDGE_VERSION)
